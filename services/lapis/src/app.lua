c9bead8f4699f9bc9c236391e9f62a7ae9a24258
Glazyrin Evgeniy <genhoi25@gmail.com>
2016-12-07 02:42:39 +0400

    Добавил реализацию Api Gateway

--- services/lapis/src/app.lua
+++ services/lapis/src/app.lua
@@ -1,8 +1,16 @@
 local lapis = require("lapis")
+local http = require("lapis.nginx.http")
 local app = lapis.Application()
 
 app:match("/", function(self)
-  return "Hello world!"
+
+    local buf = {};
+    buf[1] = http.simple("http://openresty_hello");
+    buf[2] = ", ";
+    buf[3] = http.simple("http://openresty_world");
+
+    return { table.concat(buf), status = 200, headers = {}, render = false, layout = false }
+
 end)
 
 return app
--- services/php/src/app.php
+++ services/php/src/app.php
@@ -1,4 +1,17 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+$client = new GuzzleHttp\Client();
+
+$promises = [
+    $client->getAsync('http://openresty_hello'),
+    $client->getAsync('http://openresty_world'),
+];
+/** @var \GuzzleHttp\Psr7\Response[] $results */
+$results = GuzzleHttp\Promise\unwrap($promises);
+
+$responseContent = $results[0]->getBody() . ', ' . $results[1]->getBody();
+
 http_response_code(200);
-echo "Hello World";
+echo $responseContent;
